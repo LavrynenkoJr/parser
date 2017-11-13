7dda3dfbe0a0e2abbd878733484219f733c13747
Cyborg <s.o.lavrynenko@gmail.com>
2017-11-13 16:15:46 +0200

    first commit

---
+++ src/main/java/com/cyborg/sportparser/SportparserApplication.java
@@ -0,0 +1,12 @@
+package com.cyborg.sportparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportparserApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cyborg/sportparser/SportparserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cyborg.sportparser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SportparserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
